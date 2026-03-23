f218948c4e1e666761c6ed77df0d3d3d08ad9c4d
Jessica Steinkamp <jessica@steinkamp.se>
2026-03-23 16:12:12 +0100

    initial committ

---
+++ src/main/java/com/example/user_service/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.example.user_service.controller;
+
+import com.example.user_service.entity.User;
+import com.example.user_service.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+ private UserService userService;
+
+ public UserController(UserService userService) {
+     this.userService = userService;
+ }
+
+    @GetMapping("/all")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/new")
+    public User createUser(@RequestBody User user) {
+        return userService.addUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+}
---
+++ src/main/java/com/example/user_service/service/UserService.java
@@ -0,0 +1,30 @@
+package com.example.user_service.service;
+
+import com.example.user_service.entity.User;
+import com.example.user_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).get();
+    }
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+    public void deleteUserById(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public User addUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+}
